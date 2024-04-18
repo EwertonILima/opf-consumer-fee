40c7ff1647c70fabcad530cd94716ae52e3a1a9e
ewertonilima <ewertoninaciolima@gmail.com>
2024-04-18 07:51:26 -0300

    init: :tada: Starting project

---
+++ src/main/kotlin/com/ewertonilima/consumerfee/ConsumerfeeApplication.kt
@@ -0,0 +1,11 @@
+package com.ewertonilima.consumerfee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConsumerfeeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConsumerfeeApplication>(*args)
+}
---
+++ src/test/kotlin/com/ewertonilima/consumerfee/ConsumerfeeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ewertonilima.consumerfee
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConsumerfeeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
