9f6244f8e36d585d1b1dc4bdfa1f415f6db08d7c
ewertonilima <ewertoninaciolima@gmail.com>
2024-04-18 11:21:52 -0300

    :construction: Organizing packages

--- src/main/kotlin/com/ewertonilima/consumerfee/bootloader/ConsumerfeeApplication.kt
+++ src/main/kotlin/com/ewertonilima/consumerfee/bootloader/ConsumerfeeApplication.kt
@@ -1,9 +1,11 @@
-package com.ewertonilima.consumerfee
+package com.ewertonilima.consumerfee.bootloader
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.ewertonilima.*"])
 class ConsumerfeeApplication
 
 fun main(args: Array<String>) {
